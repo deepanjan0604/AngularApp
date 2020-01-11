e13eae1f90efdb89af0fd3b8d1baf30e03e4faa1
Deepanjan <dpanjan>
2020-01-11 22:49:05 +0530

    directives

--- src/app/example.directive.ts
+++ src/app/example.directive.ts
@@ -1,13 +1,29 @@
-import { Directive,ElementRef } from '@angular/core';
+import { Directive,ElementRef,HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appExample]'
 })
 export class ExampleDirective {
 
-  constructor(elementRef:ElementRef) {
+  constructor(public elementRef:ElementRef) {
 
       elementRef.nativeElement.style.color="green";   
    }
 
+@HostBinding('style.border') border;
+
+   @HostListener('mouseover') onMouseOver(){
+    this.elementRef.nativeElement.style.backgroundColor='red';
+    this.border='5px solid black';
+     
+   }
+   @HostListener('mouseleave') onMouseLeave(){
+     this.elementRef.nativeElement.style.backgroundColor='white';
+     this.border='0px solid black';
+   }
+   numberOfClicks=0;
+   @HostListener('click', ['$event.target'])
+   onClick(btn) {
+     console.log('button', btn, 'number of clicks:', this.numberOfClicks++);
+  }
 }
